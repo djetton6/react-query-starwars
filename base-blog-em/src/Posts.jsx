7d9c6b4a98d04dc6561eb89132b750d894aac6f0
Desmend Jetton <desmendjetton@gmail.com>
2023-03-28 02:05:32 -0500

    react query init

--- base-blog-em/src/Posts.jsx
+++ base-blog-em/src/Posts.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { useQuery } from "react-query";
 
 import { PostDetail } from "./PostDetail";
 const maxPostPage = 10;
@@ -15,8 +16,16 @@ export function Posts() {
   const [selectedPost, setSelectedPost] = useState(null);
 
   // replace with useQuery
-  const data = [];
-
+  // useQuery Facts:
+  // 1. Destructures ALL the data since useQuery returns an object with data
+  // 2. use query takes 2 things(what we want to name the query, and query function and needs to be async)
+  
+  const { data, isLoading, error, isError } = useQuery("posts", fetchPosts, {
+    staleTime: 5,
+  });
+  //have to do an early return since it's async.
+  if(isLoading) return <div><h2>Loading!</h2></div>
+  if(isError) return <div><h2> We're working on it!{error}</h2></div>
   return (
     <>
       <ul>
