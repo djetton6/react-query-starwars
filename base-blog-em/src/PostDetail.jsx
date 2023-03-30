15774dc07fceb3fb8e0b7811a309972ab8ffaf9a
Desmend Jetton <desmendjetton@gmail.com>
2023-03-29 23:54:22 -0500

    updating keys

--- base-blog-em/src/PostDetail.jsx
+++ base-blog-em/src/PostDetail.jsx
@@ -1,3 +1,8 @@
+
+//Step #1 - import useQuery
+import { useQuery } from "react-query";
+import { useState } from 'react';
+
 async function fetchComments(postId) {
   const response = await fetch(
     `https://jsonplaceholder.typicode.com/comments?postId=${postId}`
@@ -22,8 +27,14 @@ async function updatePost(postId) {
 }
 
 export function PostDetail({ post }) {
-  // replace with useQuery
-  const data = [];
+  // replace with useQuery- assignmeent
+  // Step 1: add useQuery to the project
+  //Step 2, destructure the data, loading and error = useQuery['key-(usually variable', and async func]
+  //Step 3 -> make an annoynmous function () => post.id so we can't pass/
+
+  const {data, isLoading, error, isError} = useQuery('comments', () => fetchComments(post.id))
+  if(isLoading) return <h1>Loading</h1>
+  if(isError) return <h1>Error! {error}</h1>
 
   return (
     <>
