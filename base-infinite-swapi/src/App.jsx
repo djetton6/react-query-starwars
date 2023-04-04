47a80f0732553f5a5b407301711d6de5edda6455
Desmend Jetton <desmendjetton@gmail.com>
2023-04-04 02:31:40 -0500

    instlaling queryclient

--- base-infinite-swapi/src/App.jsx
+++ base-infinite-swapi/src/App.jsx
@@ -1,14 +1,21 @@
 import "./App.css";
 import { InfinitePeople } from "./people/InfinitePeople";
 import { InfiniteSpecies } from "./species/InfiniteSpecies";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+
+
+//queryClient
+const queryClient = new QueryClient()
 
 function App() {
   return (
+    <QueryClientProvider client={queryClient}>
     <div className="App">
       <h1>Infinite SWAPI</h1>
       <InfinitePeople />
       {/* <InfiniteSpecies /> */}
     </div>
+    </QueryClientProvider>
   );
 }
 
