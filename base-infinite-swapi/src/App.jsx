4909ae6f7a4e4b321e00e8f31cc8e05c2d9d317f
Desmend Jetton <desmendjetton@gmail.com>
2023-04-12 00:01:56 -0500

    installing react query dev tools

--- base-infinite-swapi/src/App.jsx
+++ base-infinite-swapi/src/App.jsx
@@ -2,7 +2,7 @@ import "./App.css";
 import { InfinitePeople } from "./people/InfinitePeople";
 import { InfiniteSpecies } from "./species/InfiniteSpecies";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
-
+import { ReactQueryDevTools } from "react-query/dev-tools";
 
 //queryClient
 const queryClient = new QueryClient()
